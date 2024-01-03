1e8d0bff3cebeb83248cb5f6c626e3a1ed2b43d6
Hazzer <warlocksforge@gmail.com>
2024-01-03 19:43:30 +0100

    init project

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BonePatches
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+
+            var harmony = new Harmony("Harmony_Bone_Patches");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Message("Bones in Medieval Overhauls patched");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Bone Patch in Medieval Overhaul exception: {e.Message}");
+            }
+        }
+
+    }
+}
---
+++ Source/RemoveBoneSpawnPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BonePatches
+{
+    [HarmonyPatch(typeof(Pawn))]
+    [HarmonyPatch("ButcherProducts")]
+    public class RemoveBoneSpawnPatch
+    {
+
+        [HarmonyAfter(new string[] { "lalapyhon.rimworld.medievaloverhaul" })]
+        public static IEnumerable<Thing> Postfix(IEnumerable<Thing> result, Pawn butcher, float efficiency)
+        {
+            foreach (Thing t in result)
+            {
+                if (t.def.defName != "DankPyon_Bone")
+                {
+                    yield return t;
+                }
+            }
+        }
+    }
+}
